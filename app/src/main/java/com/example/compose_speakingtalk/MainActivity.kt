2dde0305050ecce3b99465835e78e193ff43434f
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2024-04-23 16:08:08 +0900

    feat: NotificationListenerService를 사용해 카카오톡 알림을 가져오도록 구현

--- app/src/main/java/com/example/compose_speakingtalk/MainActivity.kt
+++ app/src/main/java/com/example/compose_speakingtalk/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.compose_speakingtalk
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,26 +20,44 @@ import java.util.Locale
 class MainActivity : ComponentActivity() {
 
     private val textToSpeech: TextToSpeech by lazy {
-        TextToSpeech(this, TextToSpeech.OnInitListener {
+        TextToSpeech(this) {
             if (it != TextToSpeech.ERROR) {
                 textToSpeech.setLanguage(Locale.KOREAN)
             }
-        })
+        }
+    }
+
+    private val kakaoReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if(intent?.action.equals(NotificationListener.ACTION_TAG)) {
+                val sender = intent?.getStringExtra("sender")
+                val content = intent?.getStringExtra("content")
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposespeakingtalkTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    OnOffScreen()
                 }
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(kakaoReceiver, IntentFilter(NotificationListener.ACTION_TAG),
+            RECEIVER_NOT_EXPORTED
+        )
+    }
+
 }
 
 
---
+++ app/src/main/java/com/example/compose_speakingtalk/NotificationListener.kt
@@ -0,0 +1,45 @@
+package com.example.compose_speakingtalk
+
+import android.content.Intent
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+import androidx.core.app.NotificationCompat
+
+class NotificationListener : NotificationListenerService() {
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        Log.e("speakingtalk", "MyNotificationListener.onListenerConnected()")
+    }
+
+    override fun onListenerDisconnected() {
+        super.onListenerDisconnected()
+        Log.e("speakingtalk", "MyNotificationListener.onListenerDisconnected()")
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+
+        if (sbn?.packageName.equals("com.kakao.talk")) {
+            val bundle = sbn?.notification?.extras
+            if (bundle != null) {
+                val sender = bundle.getString(NotificationCompat.EXTRA_TITLE) + "님이 카톡을 보냈습니다."
+                val content = bundle.getString(NotificationCompat.EXTRA_TEXT) ?: ""
+                sendToActivity(sender, content)
+            }
+        }
+    }
+
+    private fun sendToActivity(sender: String, content: String) {
+        val intent = Intent(ACTION_TAG).apply {
+            putExtra("sender", sender)
+            putExtra("content", content)
+        }
+        sendBroadcast(intent)
+    }
+
+    companion object {
+        const val ACTION_TAG = "com.notificationListenerService"
+    }
+}
